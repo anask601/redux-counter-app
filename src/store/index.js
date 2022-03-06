238693001576f4e6751d30d6d3323f6912c93d41
anask601 <anask60155@gmail.com>
2022-03-06 23:41:17 +0530

    added functionality of increment and decrement

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+import React from "react";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
